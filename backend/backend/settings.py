02e4c9b2d5a835f5bde65f80874eaeb7255facc0
Veerla Vijay Kumar Yadav <vijaykumarveerla3377@gmail.com>
2026-02-04 11:52:59 +0530

    Fix typo in STATIC_URL definition

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+TATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR.parent / 'frontend']
 
 # Default primary key field type
